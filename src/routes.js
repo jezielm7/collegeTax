4747989a6d3b633a8a4138b31c740edb94bfb2d2
Jeziel Marques <jezielmarques7@gmail.com>
2020-05-27 18:32:01 -0300

    updated structure

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,14 @@ import React from 'react';
 import { View } from 'react-native';
 
 export default function Routes() {
-  return <View />;
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="List" component={List} />
+        <Stack.Screen name="Register" component={Register} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
 
