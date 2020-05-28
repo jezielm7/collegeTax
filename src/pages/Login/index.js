7a2a4da40d260e323949f047d3c470f4dc727f3d
Jeziel Marques <jezielmarques7@gmail.com>
2020-05-28 15:26:11 -0300

    created login screen

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,7 +1,87 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Text, StyleSheet, TextInput, TouchableOpacity, StatusBar, } from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
 
-export default function Login() {
-  return <View />;
+export default function Login({ navigation }) {
+  return (
+    <LinearGradient colors={['#5700D8', '#6100BF', '#8300C0']} style={styles.container}>
+      <StatusBar backgroundColor="#5700D8" />
+      <Text style={styles.textTitle}>College Tax</Text>
+
+      <View style={styles.loginView}>
+        <Text style={styles.loginTxt}>Login</Text>
+
+        <TextInput
+          style={styles.input}
+          placeholder="Email"
+          autoCapitalize="none"
+          autoCorrect={false}
+          placeholderTextColor="#eee"
+        />
+
+        <TextInput
+          style={styles.input}
+          placeholder="Password"
+          keyboardType="number-pad"
+          secureTextEntry
+          placeholderTextColor="#eee"
+        />
+      </View>
+
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => navigation.navigate('List')}
+      >
+        <Text style={styles.btnText} >Entrar</Text>
+      </TouchableOpacity>
+    </LinearGradient>
+  );
 }
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  textTitle: {
+    top: '10%',
+    fontSize: 32,
+    color: '#fff',
+    textAlign: 'center',
+  },
+  loginView: {
+    flex: 1,
+    top: '24%',
+  },
+  loginTxt: {
+    fontSize: 30,
+    color: '#fff',
+    textAlign: 'center',
+    marginBottom: '15%',
+  },
+  input: {
+    padding: 10,
+    width: '80%',
+    fontSize: 16,
+    color: '#fff',
+    borderRadius: 8,
+    marginBottom: 15,
+    alignSelf: 'center',
+    borderColor: '#ddd',
+    borderWidth: 0.8,
+  },
+  button: {
+    height: 50,
+    width: '80%',
+    bottom: '20%',
+    borderRadius: 8,
+    alignSelf: 'center',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderColor: '#ddd',
+    borderWidth: 0.8,
+  },
+  btnText: {
+    fontSize: 18,
+    color: '#fff',
+  },
+});
--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,21 @@
 import React from 'react';
-import { View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import List from './pages/List';
+import Login from './pages/Login';
+import Register from './pages/Login';
+
+const Stack = createStackNavigator();
 
 export default function Routes() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Login">
-        <Stack.Screen name="Login" component={Login} />
-        <Stack.Screen name="List" component={List} />
-        <Stack.Screen name="Register" component={Register} />
+        <Stack.Screen name="List" component={List} options={{ headerShown: false }} />
+        <Stack.Screen name="Login" component={Login} options={{ headerShown: false }} />
+        <Stack.Screen name="Register" component={Register}  options={{ headerShown: false }} />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
-
